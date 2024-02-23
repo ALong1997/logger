c2a69bd17035109471a7f4df0f47802c3a2cabe6
Benjamin <cl1026931058@163.com>
2024-02-23 18:29:04 +0800

    exportable logger

--- logger.go
+++ logger.go
@@ -4,7 +4,7 @@ import (
 	"go.uber.org/zap"
 )
 
-func getLogger() *zap.SugaredLogger {
+func GetLogger() *zap.SugaredLogger {
 	if globalLogger.withGoID {
 		return globalLogger.With(zap.String(goIDKey, GoID()))
 	}
@@ -17,33 +17,33 @@ func Sync() error {
 }
 
 func Debug(args ...interface{}) {
-	getLogger().Debug(args)
+	GetLogger().Debug(args)
 }
 
 func Info(args ...interface{}) {
-	getLogger().Info(args)
+	GetLogger().Info(args)
 }
 
 func Warn(args ...interface{}) {
-	getLogger().Warn(args)
+	GetLogger().Warn(args)
 }
 
 func Error(args ...interface{}) {
-	getLogger().Error(args)
+	GetLogger().Error(args)
 }
 
 func DebugF(format string, args ...interface{}) {
-	getLogger().Debugf(format, args)
+	GetLogger().Debugf(format, args)
 }
 
 func InfoF(format string, args ...interface{}) {
-	getLogger().Infof(format, args)
+	GetLogger().Infof(format, args)
 }
 
 func WarnF(format string, args ...interface{}) {
-	getLogger().Warnf(format, args)
+	GetLogger().Warnf(format, args)
 }
 
 func ErrorF(format string, args ...interface{}) {
-	getLogger().Errorf(format, args)
+	GetLogger().Errorf(format, args)
 }
--- logger_with_context.go
+++ logger_with_context.go
@@ -7,44 +7,44 @@ import (
 
 const TraceIDKey = "trace_id"
 
-func getLoggerWithContext(ctx context.Context) *zap.SugaredLogger {
+func GetLoggerWithContext(ctx context.Context) *zap.SugaredLogger {
 	if ctx != nil {
 		if traceID, ok := ctx.Value(TraceIDKey).(string); ok {
-			return getLogger().With(zap.String(TraceIDKey, traceID))
+			return GetLogger().With(zap.String(TraceIDKey, traceID))
 		}
 	}
 
-	return getLogger()
+	return GetLogger()
 }
 
 func DebugWithContext(ctx context.Context, args ...interface{}) {
-	getLoggerWithContext(ctx).Debug(args)
+	GetLoggerWithContext(ctx).Debug(args)
 }
 
 func InfoWithContext(ctx context.Context, args ...interface{}) {
-	getLoggerWithContext(ctx).Info(args)
+	GetLoggerWithContext(ctx).Info(args)
 }
 
 func WarnWithContext(ctx context.Context, args ...interface{}) {
-	getLoggerWithContext(ctx).Warn(args)
+	GetLoggerWithContext(ctx).Warn(args)
 }
 
 func ErrorWithContext(ctx context.Context, args ...interface{}) {
-	getLoggerWithContext(ctx).Error(args)
+	GetLoggerWithContext(ctx).Error(args)
 }
 
 func DebugFWithContext(ctx context.Context, format string, args ...interface{}) {
-	getLoggerWithContext(ctx).Debugf(format, args)
+	GetLoggerWithContext(ctx).Debugf(format, args)
 }
 
 func InfoFWithContext(ctx context.Context, format string, args ...interface{}) {
-	getLoggerWithContext(ctx).Infof(format, args)
+	GetLoggerWithContext(ctx).Infof(format, args)
 }
 
 func WarnFWithContext(ctx context.Context, format string, args ...interface{}) {
-	getLoggerWithContext(ctx).Warnf(format, args)
+	GetLoggerWithContext(ctx).Warnf(format, args)
 }
 
 func ErrorFWithContext(ctx context.Context, format string, args ...interface{}) {
-	getLoggerWithContext(ctx).Errorf(format, args)
+	GetLoggerWithContext(ctx).Errorf(format, args)
 }
